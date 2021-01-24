d3bee76a6e8e711dfd5c95413ddf2fa9245d7ed9
i7242 <xingyuyan@outlook.com>
2021-01-24 13:14:16 -0500

    support data type and added data

--- simple_test.jl
+++ simple_test.jl
@@ -6,11 +6,14 @@ Try to implement some simple SQL like queries using Julia
 
 """
 Simple table structure, will continue add fields
-* will add column data type and data
+* consider to removed header and type
+* they can be achieved by using NamedTuple with eltype()
 """
 struct Table
     title::String
-    headers::Tuple
+    headers::Tuple{Vararg{Symbol}}
+    types::Tuple{Vararg{DataType}}
+    data::NamedTuple
 end
 
 """
@@ -20,11 +23,14 @@ Initial test to use macro
 """
 macro CREATE(expr)
     table = expr.args[1]
-    name = String(expr.args[1])
-    headers = Tuple(expr.args[2:end])
+    title = String(expr.args[1])
+    headers = Tuple([eval(ex)[1] for ex in expr.args[2:end]])
+    types = Tuple([eval(ex)[2] for ex in expr.args[2:end]])
+    columns = Tuple([type[] for type in types])
+    data = (; zip(headers, columns)...)
     esc(
         quote
-            $table = Table($name, $headers)
+            $table = Table($title, $headers, $types, $data)
         end
     )
 end
@@ -34,6 +40,25 @@ Usage:
 * create a table of name "users"
 * the headers are: "id", "name", and "type"
 """
-@CREATE users, id, name, type
+@CREATE users, (:id, Int64), (:name, String), (:type, String)
 
+typeof(users.data)
+eltype(users.data.id)
 varinfo()
+
+#=
+macro TestInputs(expr)
+    println(expr)
+    println(expr.head)
+    println(expr.args[1])
+    println(typeof(expr.args[1]))
+    println(expr.args[2])
+    println(typeof(expr.args[2]))
+    println(expr.args[3])
+    println(typeof(expr.args[3]))
+    println(expr.args[3].args[2])
+    println(typeof(expr.args[3].args[2]))
+end
+
+@TestInputs name, :id, (age, :type)
+=#
