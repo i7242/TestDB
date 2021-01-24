6e1df9ef10f7ae2f5e0158cd8b9f54dad709f03d
i7242 <xingyuyan@outlook.com>
2021-01-24 00:14:56 -0500

    add headers, no data type yet

--- simple_test.jl
+++ simple_test.jl
@@ -1,24 +1,39 @@
-# Prepare data structure for database & query
-
 #=
-    Simple table structure, will continue add fields
+Try to implement some simple SQL like queries using Julia
+* the objective is do some exercise with metaprogramming
+* just for test, will only support some basic query like select
 =#
+
+"""
+Simple table structure, will continue add fields
+* will add column data type and data
+"""
 struct Table
     title::String
+    headers::Tuple
 end
 
-#=
-    Initial test to use macro
-=#
-macro CREATE(input::Symbol)
-    name = String(input)
+"""
+Initial test to use macro
+* the first variable will be used as table name
+* and the rests are headers
+"""
+macro CREATE(expr)
+    table = expr.args[1]
+    name = String(expr.args[1])
+    headers = Tuple(expr.args[2:end])
     esc(
         quote
-            $input = Table($name)
+            $table = Table($name, $headers)
         end
     )
 end
 
-@CREATE lololo
+"""
+Usage:
+* create a table of name "users"
+* the headers are: "id", "name", and "type"
+"""
+@CREATE users, id, name, type
 
 varinfo()
